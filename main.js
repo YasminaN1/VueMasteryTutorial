abbdce78d9851220c51ddf608fffe259a541adc9
Yasmina Nigmanova <526ynigmanova@frhsd.com>
2025-11-26 12:23:53 -0500

    List Rendering

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ data() {
     return{
         product: 'Socks',
         image: './assets/images/socks_blue.jpg',
-        inventory: 0
+        inventory: 0,
+        inStock: true,
+details: ['50% cotton', '30% wool', '20% polyester'],
+variants: [
+    { id: 2234, color: 'green'},
+    {id: 2235, color: 'blue'},
+]
     }
 }
 })
